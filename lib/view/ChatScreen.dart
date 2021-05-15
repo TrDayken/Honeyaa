ddbb8b7b759200c926b69950da4357cf01390ac9
TrDayken <tranhdayken@gmail.com>
2021-05-15 11:12:13 +0700

    [ADD] ChatScreen and ProfileScreen

--- lib/view/ChatScreen.dart
+++ lib/view/ChatScreen.dart
@@ -19,21 +19,32 @@ class _ChatScreenState extends State<ChatScreen>{
         icon: new Icon(
           Icons.arrow_back,
           color: Colors.grey,
-          size: 40.0,
+          size: 30.0,
         ),
         onPressed: () {
           Navigator.pop(context);
           }
-      )
+      ),
 
-      //actions: <Widget>[],
+      actions: <Widget>[
+        new IconButton(
+          icon : new Icon( 
+            Icons.shield,
+            color: Colors.blue,
+            size: 30.0,
+          ),
+        onPressed: (){
+          //TODO: implement block/ unblock
+        },
+        )
+      ],
 
     );
   }
 
   Widget _buildBottomBar() {
     return BottomAppBar(
-      // color: Colors.transparent,
+      // color: Colors.blue,
       // elevation: 0.0,
       // child: new Padding( 
       //   padding: const EdgeInsets.all(16.0),
@@ -50,11 +61,112 @@ class _ChatScreenState extends State<ChatScreen>{
 
   @override 
   Widget build(BuildContext context) {
+    bool isSenderChatBubble = true ; 
     return Scaffold(
       appBar: _buildAppBar(),
-
       // TODO: Implement chat bubble
-      
+      body: Stack(
+        children: <Widget>[
+          Container(
+            width: MediaQuery.of(context).size.width,
+            height: MediaQuery.of(context).size.height,
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                begin: Alignment.topCenter,
+                end: Alignment.bottomCenter,
+                colors: [
+                  Color.fromARGB(255, 160, 20, 177),
+                  Color.fromARGB(255, 194, 142, 20)
+                ]
+              )
+            )
+          ),
+
+          Container(
+            child: Container(
+              height: MediaQuery.of(context).size.height,
+              alignment: Alignment.topCenter,
+              child: LayoutBuilder(
+                builder: (BuildContext context,
+                BoxConstraints viewportConstraints) {
+                  return SingleChildScrollView(
+                    physics: ClampingScrollPhysics(),
+                    child: ConstrainedBox(
+                      constraints: BoxConstraints (
+                        minHeight: viewportConstraints.maxHeight,
+                      ),
+                      child: IntrinsicHeight(
+                        //child: Column(children: _multicolorbubbles(),)
+                        )
+                      ),
+                  );
+                }
+                ),  
+              )
+          ),
+          
+          ColorFiltered(
+            colorFilter: ColorFilter.mode(
+                Colors.white, isSenderChatBubble ? BlendMode.dstATop : BlendMode.srcOut), //config isSendersChatBubble
+            child: Container(
+              alignment: isSenderChatBubble ? Alignment.centerLeft :Alignment.centerRight, // config ismine?
+              color: Colors.transparent,
+              child: Stack(
+                children: [
+                  Container(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Container(
+                          child: Row(
+                            mainAxisSize: MainAxisSize.min,
+                            children: [
+                              Container(
+                                alignment: isSenderChatBubble //config isSendersChatBubble
+                                ? Alignment.centerLeft
+                                : Alignment.centerRight,
+                                
+                                child: isSenderChatBubble //config isSendersChatBubble
+                                ? Row(children: [
+                                  Center(
+                                    child: Container(
+                                      constraints: BoxConstraints(maxWidth: 250, minWidth: 55),
+                                      padding: EdgeInsets.fromLTRB(15,20,15,3),
+                                      child: Text("okay", style: TextStyle(
+                                        color: Colors.transparent,
+                                      ))
+                                    )
+                                  )
+                                ],
+                              )
+                              : Center(
+                                child: Container(
+                                constraints: BoxConstraints(maxWidth: 250,),
+                                padding: EdgeInsets.fromLTRB(15,20,15,3),
+                                child: Text("hello?", style: TextStyle(
+                                  color: Colors.transparent),
+                                             
+                                ),
+                                )),
+                              margin: EdgeInsets.only(top: 10, right: 20, left: 16),
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(20.0),
+                                color: Colors.grey.withOpacity(0.9),
+                              ),
+                            )
+                          ],
+                        ),
+                      )
+                    ],
+                  )
+                )
+              ],
+            )
+            ),
+          )
+        ],
+      ),
+
       bottomNavigationBar: _buildBottomBar(),
     );
   }
---
+++ lib/view/ProfileScreen.dart
@@ -0,0 +1,21 @@
+import 'dart:html';
+
+import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/rendering.dart';
+
+
+class ProfileScreen extends StatefulWidget {
+  ProfileScreen({Key key}) : super(key: key);
+
+  @override 
+  _ProfileScreenState createState() => _ProfileScreenState();
+}
+
+class _ProfileScreenState extends State<ProfileScreen> 
+{
+  @override
+  Widget build(BuildContext context) 
+  {
+  }
+}
