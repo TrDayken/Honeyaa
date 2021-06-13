31fb7b64c104e4e502e66d5ce935a0533c7fadec
Đặng Ngọc Duy <49876587+pmn2wodee@users.noreply.github.com>
2021-06-13 09:36:41 +0700

    [UPDATE] fix ios

--- ios/Runner/AppDelegate.m
+++ ios/Runner/AppDelegate.m
@@ -1,16 +1,21 @@
-#import "AppDelegate.h"
-#import "GeneratedPluginRegistrant.h"
-#import "Firebase.h"
+#include "AppDelegate.h"
+#include "GeneratedPluginRegistrant.h"
+
+@import Firebase; // new
 
 @implementation AppDelegate
 
+
 - (BOOL)application:(UIApplication *)application
-    didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-    [FIRApp configure];
-  [GeneratedPluginRegistrant registerWithRegistry:self];
-  // Override point for customization after application launch.
-  
-  return [super application:application didFinishLaunchingWithOptions:launchOptions];
-}
+didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+
+//configure firebase
+[FIRApp configure]; // new
+
+[GeneratedPluginRegistrant registerWithRegistry:self];
+// Override point for customization after application launch.
+return [super application:application didFinishLaunchingWithOptions:launchOptions];
+
+  }
 
 @end
