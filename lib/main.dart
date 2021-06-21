0e8ca260aad40b5cf78a2fbf96831111c6d51a17
Đặng Ngọc Duy <49876587+pmn2wodee@users.noreply.github.com>
2021-06-21 18:25:20 +0700

    [UPDATE] mainhub

--- lib/main.dart
+++ lib/main.dart
@@ -22,7 +22,7 @@ class MyApp extends StatelessWidget {
         primaryColorBrightness: Brightness.light,
         primarySwatch: Colors.blue,
       ),
-      initialRoute: '/register',
+      initialRoute: '/mainhub',
       onGenerateRoute: RouteHandler.generateRoute,
     );
   }
--- lib/view/MainScreen.dart
+++ lib/view/MainScreen.dart
@@ -1,16 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_tindercard/flutter_tindercard.dart';
 import 'package:honeyaa_clientside/view/ProfileScreen.dart';
 
 import '../component//RoundIconButton.dart';
 import 'package:honeyaa_clientside/auth/Methods.dart';
-import '../component/cards.dart';
-import '../component/matches.dart';
-import '../component/profiles.dart';
-
-final MatchEngine matchEngine = new MatchEngine(
-    matches: demoProfiles.map((Profile profile) {
-  return Match(profile: profile);
-}).toList());
 
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
@@ -22,41 +15,16 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  Match match = new Match();
+  List itemTemp = [];
+  int itemLength = 0;
 
-  Widget _buildAppBar() {
-    return AppBar(
-      backgroundColor: Colors.transparent,
-      elevation: 0.0,
-      centerTitle: true,
-      leading: new IconButton(
-        icon: new Icon(
-          Icons.person,
-          color: Colors.grey,
-          size: 40.0,
-        ),
-        onPressed: () => {
-          // logOut(context)
-          Navigator.pushNamed(context, '/profile')
-        },
-      ),
-      title: new FlutterLogo(
-        size: 30.0,
-        textColor: Colors.red,
-      ),
-      actions: <Widget>[
-        new IconButton(
-          icon: new Icon(
-            Icons.chat_bubble,
-            color: Colors.grey,
-            size: 40.0,
-          ),
-          onPressed: () {
-            Navigator.pushNamed(context, '/chatscreen');
-          },
-        ),
-      ],
-    );
+  @override
+  void initState() {
+    super.initState();
+    setState(() {
+      //itemTemp = explore_json;
+      //itemLength = explore_json.lenght;
+    });
   }
 
   Widget _buildBottomBar() {
@@ -77,21 +45,21 @@ class _MyHomePageState extends State<MyHomePage> {
                 icon: Icons.clear,
                 iconColor: Colors.red,
                 onPressed: () {
-                  matchEngine.currentMatch.nope();
+                  // matchEngine.currentMatch.nope();
                 },
               ),
               new RoundIconButton.small(
                 icon: Icons.star,
                 iconColor: Colors.blue,
                 onPressed: () {
-                  matchEngine.currentMatch.superLike();
+                  // matchEngine.currentMatch.superLike();
                 },
               ),
               new RoundIconButton.large(
                 icon: Icons.favorite,
                 iconColor: Colors.green,
                 onPressed: () {
-                  matchEngine.currentMatch.like();
+                  // matchEngine.currentMatch.like();
                 },
               ),
               new RoundIconButton.small(
@@ -104,13 +72,213 @@ class _MyHomePageState extends State<MyHomePage> {
         ));
   }
 
+  Widget getbody() {
+    var size = MediaQuery.of(context).size;
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 20),
+      child: Container(
+        height: size.height,
+        child: TinderSwapCard(
+          totalNum: 10,
+          maxHeight: size.height * 0.7,
+          maxWidth: size.width,
+          minWidth: size.width * 0.75,
+          minHeight: size.height * 0.5,
+          cardBuilder: (context, index) => Container(
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                  boxShadow: [
+                    BoxShadow(
+                        color: Colors.grey.withOpacity(0.3),
+                        blurRadius: 5,
+                        spreadRadius: 2)
+                  ]),
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(10),
+                child: Stack(
+                  children: [
+                    Container(
+                      width: size.width,
+                      height: size.height,
+                      decoration: BoxDecoration(
+                          image: DecorationImage(
+                              image: NetworkImage(
+                                  'https://scontent-hkg4-2.xx.fbcdn.net/v/t31.18172-8/10841892_125844497760765_5091665117199786065_o.jpg?_nc_cat=110&ccb=1-3&_nc_sid=ba80b0&_nc_ohc=dGiNk41WMpoAX_ymzGe&_nc_ht=scontent-hkg4-2.xx&oh=6a0cadfcc2c559b922f41ba3ad6c3135&oe=60F44D36'),
+                              fit: BoxFit.cover)),
+                    ),
+                    Container(
+                      width: size.width,
+                      height: size.height,
+                      decoration: BoxDecoration(
+                          gradient: LinearGradient(
+                              colors: [
+                            Colors.black.withOpacity(0.25),
+                            Colors.black.withOpacity(0)
+                          ],
+                              end: Alignment.topCenter,
+                              begin: Alignment.bottomCenter)),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        children: [
+                          Padding(
+                              padding: const EdgeInsets.all(15),
+                              child: Row(
+                                children: [
+                                  Container(
+                                    width: size.width * 0.72,
+                                    child: Column(
+                                      children: [
+                                        Row(
+                                          children: [
+                                            Text(
+                                              "Cuong",
+                                              style: TextStyle(
+                                                  fontSize: 24,
+                                                  color: Colors.white,
+                                                  fontWeight: FontWeight.bold),
+                                            ),
+                                            SizedBox(
+                                              width: 10,
+                                            ),
+                                            Text(
+                                              "21",
+                                              style: TextStyle(
+                                                  fontSize: 24,
+                                                  color: Colors.white),
+                                            ),
+                                          ],
+                                        ),
+                                        SizedBox(
+                                          height: 10,
+                                        ),
+                                        Row(
+                                          children: [
+                                            Container(
+                                              width: 10,
+                                              height: 10,
+                                              decoration: BoxDecoration(
+                                                  color: Colors.green,
+                                                  shape: BoxShape.circle),
+                                            ),
+                                            SizedBox(
+                                              width: 10,
+                                            ),
+                                            Text(
+                                              "Recently Active",
+                                              style: TextStyle(
+                                                  fontSize: 16,
+                                                  color: Colors.white),
+                                            )
+                                          ],
+                                        ),
+                                        SizedBox(
+                                          height: 10,
+                                        ),
+                                        Row(
+                                            children: List.generate(
+                                          //itemTemp[index]
+                                          //['favorites'].length,
+                                          //(index)
+                                          3,
+                                          (index) {
+                                            //if(indexFavorites == 0){
+                                            return Padding(
+                                                padding: const EdgeInsets.only(
+                                                    right: 8),
+                                                child: Container(
+                                                  decoration: BoxDecoration(
+                                                      borderRadius:
+                                                          BorderRadius.circular(
+                                                              30),
+                                                      color: Colors.white
+                                                          .withOpacity(0.2),
+                                                      border: Border.all(
+                                                          color: Colors.white,
+                                                          width: 2)),
+                                                  child: Padding(
+                                                    padding:
+                                                        const EdgeInsets.only(
+                                                            left: 10,
+                                                            right: 10,
+                                                            bottom: 3,
+                                                            top: 3),
+                                                    child: Text(
+                                                      //itemTemp
+                                                      //[index]
+                                                      //['favorites']
+                                                      //[indexFavorites],
+                                                      "Sport",
+                                                      style: TextStyle(
+                                                          color: Colors.white),
+                                                    ),
+                                                  ),
+                                                ));
+                                            //}
+                                            return Padding(
+                                                padding: const EdgeInsets.only(
+                                                    right: 8),
+                                                child: Container(
+                                                  decoration: BoxDecoration(
+                                                      borderRadius:
+                                                          BorderRadius.circular(
+                                                              30),
+                                                      color: Colors.white
+                                                          .withOpacity(0.4),
+                                                      border: Border.all(
+                                                          color: Colors.white,
+                                                          width: 2)),
+                                                  child: Padding(
+                                                    padding:
+                                                        const EdgeInsets.only(
+                                                            left: 10,
+                                                            right: 10,
+                                                            bottom: 3,
+                                                            top: 3),
+                                                    child: Text(
+                                                      //itemTemp
+                                                      //[index]
+                                                      //['favorites']
+                                                      //[indexFavorites],
+                                                      "Sport",
+                                                      style: TextStyle(
+                                                          color: Colors.white),
+                                                    ),
+                                                  ),
+                                                ));
+                                          },
+                                        ))
+                                      ],
+                                    ),
+                                  ),
+                                  Expanded(
+                                      child: Container(
+                                    width: size.width * 0.2,
+                                    child: Center(
+                                      child: Icon(
+                                        Icons.info,
+                                        color: Colors.white,
+                                        size: 28,
+                                      ),
+                                    ),
+                                  ))
+                                ],
+                              ))
+                        ],
+                      ),
+                    )
+                  ],
+                ),
+              )),
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      // appBar: _buildAppBar(),
-      // body: new CardStack(
-      //   //matchEngine: matchEngine,
-      // ),
+      body: getbody(),
       bottomNavigationBar: _buildBottomBar(),
     );
   }
